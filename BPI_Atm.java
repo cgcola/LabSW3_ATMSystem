ef0f955fe29d9e775bcf256dcc2a23bb0b8e78d4
Carl Geneson C. Ola <ocarlgeneson@gmail.com>
2026-02-24 01:04:52 +0000

    Update Code

---
+++ BPI_Atm.java
@@ -0,0 +1,11 @@
+public class BPI_Atm {
+    public static void main(String[] args) {
+        ATMDispenseChain atmDispenser = new ATMDispenseChain();
+        int amount = 2890; // Amount to be dispensed
+        if (amount % 10 != 0) {
+            System.out.println("Amount should be in multiples of 10s.");
+            
+        } else
+        atmDispenser.dispense(new Currency(amount));
+    }
+}
